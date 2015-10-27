062396cbfd0a9d8a8461f80a409346be8b499d3e
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-10-27 01:21:44 +0100

    Wrap state functions in an enum
    We can't compare them without having an enum or something similar, which
    is kind of stupid, but oh well.

--- src/java/com/hypirion/subtex/Lexer.java
+++ src/java/com/hypirion/subtex/Lexer.java
@@ -2,6 +2,7 @@ package com.hypirion.subtex;
 
 import clojure.lang.Keyword;
 import java.util.function.Function;
+import java.util.function.BiFunction;
 
 // DFA of a subset of LaTeX.
 class Lexer {
@@ -28,21 +29,38 @@ class Lexer {
         // TODO? Math
     }
 
+    private enum StateFn {
+        Begin((a, b) -> a.stateBegin(b)),
+        Slash0((a, b) -> a.stateSlash0(b)),
+        Slash((a, b) -> a.stateSlash(b)),
+        InComment((a, b) -> a.stateInComment(b)),
+        Text((a, b) -> a.stateText(b)),
+        TextNewline((a, b) -> a.stateTextNewline(b)),
+        Newline0((a, b) -> a.stateNewline0(b)),
+        Newlines((a, b) -> a.stateNewlines(b));
+
+        StateFn(BiFunction<Lexer, Character, State> bi) {
+            this.fn = bi;
+        }
+
+        BiFunction<Lexer, Character, State> fn;
+    }
+
     Token token;
-    Function<Character, State> state;
+    StateFn state;
     int backtrack;
 
     Lexer() {
-        state = this::stateBegin;
+        state = StateFn.Begin;
         backtrack = 0;
     }
 
     State state(Character c) {
-        return state.apply(c);
+        return state.fn.apply(this, c);
     }
 
     void reset() {
-        state = this::stateBegin;
+        state = StateFn.Begin;
         backtrack = 0;
     }
 
@@ -53,7 +71,7 @@ class Lexer {
     State stateBegin(Character c) {
         switch (c) {
         case '\n':
-            state = this::stateNewline0;
+            state = StateFn.Newline0;
             return State.Continue;
         case '{':
             token = Token.OpenBrace;
@@ -62,20 +80,20 @@ class Lexer {
             token = Token.CloseBrace;
             return State.End;
         case '\\':
-            state = this::stateSlash0;
+            state = StateFn.Slash0;
             return State.Continue;
         case '%':
-            state = this::stateInComment;
+            state = StateFn.InComment;
             return State.Continue;
         default:
-            state = this::stateText;
+            state = StateFn.Text;
             return State.Continue;
         }
     }
 
     State stateSlash0(Character c) {
         if (Character.isLetter(c)) {
-            state = this::stateSlash;
+            state = StateFn.Slash;
             return State.Continue;
         }
         // Token may or may not be unexpected; this is up to the user.
@@ -95,7 +113,7 @@ class Lexer {
 
     State stateInComment(Character c) {
         if (c == '\n') {
-            state = this::stateBegin;
+            state = StateFn.Begin;
             token = Token.Comment;
             backtrack = 1;
             return State.End;
@@ -110,7 +128,7 @@ class Lexer {
             backtrack = 1;
             return State.End;
         case '\n':
-            state = this::stateTextNewline;
+            state = StateFn.TextNewline;
             return State.Continue;
         }
         return State.Continue;
@@ -127,7 +145,7 @@ class Lexer {
             backtrack = 2;
             return State.End;
         }
-        state = this::stateText;
+        state = StateFn.Text;
         return State.Continue;
     }
 
@@ -138,11 +156,11 @@ class Lexer {
             backtrack = 1;
             return State.End;
         case '\n':
-            state = this::stateNewlines;
+            state = StateFn.Newlines;
             return State.Continue;
 
         }
-        state = this::stateBegin;
+        state = StateFn.Begin;
         return State.Continue;
     }
 
